249053fc72e79236e2e3b7b75fe2b7434df75196
samuelselasi <samelsekasi@gmail.com>
2023-06-29 11:53:28 +0000

    Add query for AND and BETWEEN 3

---
+++ intro_to_data_science/3_and_between.sql
@@ -0,0 +1,4 @@
+-- When you use the BETWEEN operator in SQL, do the results include the values of your endpoints, or not? Figure out the answer to this important question by writing a query that displays the order date and gloss_qty data for all orders where gloss_qty is between 24 and 29. Then look at your output to see if the BETWEEN operator included the begin and end values or not.
+SELECT occurred_at, gloss_qty
+ FROM orders
+  WHERE gloss_qty BETWEEN 24 AND 29;

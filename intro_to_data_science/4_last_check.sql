4b423c0fb8384f88d45794ee5558b347b6174972
samuelselasi <samelsekasi@gmail.com>
2023-06-30 23:18:23 +0000

    Add last check quiz 4

---
+++ intro_to_data_science/4_last_check.sql
@@ -0,0 +1,7 @@
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+SELECT region.name RegionName, accounts.name AccountName, orders.total_amt_usd/(orders.total + 0.01) UnitPrice
+FROM region
+JOIN sales_reps ON region.id = sales_reps.region_id
+JOIN accounts ON sales_reps.id = accounts.sales_rep_id
+JOIN orders ON accounts.id = orders.account_id
+WHERE orders.standard_qty > 100;

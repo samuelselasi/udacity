bc7a3ee39711875701b4beb60e488ed44d68af21
samuelselasi <samelsekasi@gmail.com>
2023-07-10 00:19:07 +0000

    Add having  quiz task6

---
+++ intro_to_data_science/6_having.sql
@@ -0,0 +1,8 @@
+-- Which account has spent the most with us?
+SELECT DISTINCT accounts.name, orders.account_id, SUM(orders.total_amt_usd) total_across_all_orders
+FROM accounts
+JOIN orders ON accounts.id = orders.account_id
+GROUP BY accounts.name, orders.account_id
+HAVING SUM(orders.total_amt_usd) > 1000
+ORDER BY SUM(orders.total_amt_usd) DESC
+LIMIT 1;
